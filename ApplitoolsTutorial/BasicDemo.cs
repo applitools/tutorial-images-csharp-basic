be5928e8a429176e6c1a6ccc9c6db498eee88426
Itai Bar-Haim <itai.barhaim@applitools.com>
2025-04-23 10:10:35 +0300

    fixed code for .net 8.0

--- ApplitoolsTutorial/BasicDemo.cs
+++ ApplitoolsTutorial/BasicDemo.cs
@@ -1,9 +1,5 @@
 ﻿using Applitools.Images;
 using NUnit.Framework;
-using System;
-using System.Drawing;
-using System.IO;
-using System.Net;
 
 namespace ApplitoolsTutorial
 {
@@ -22,11 +18,8 @@ namespace ApplitoolsTutorial
             // Start the session and set app name and test name.
             eyes.Open("Demo App - Images C#", "Smoke Test - Images C#");
 
-            // Load page image and validate.
-            Bitmap bitmap = GetImage(new Uri("https://i.ibb.co/bJgzfb3/applitools.png"));
-
             // Visual checkpoint.
-            eyes.CheckImage(bitmap);
+            eyes.Check(Target.Url("https://i.ibb.co/bJgzfb3/applitools.png"));
 
             // End the test.
             eyes.Close();
@@ -36,20 +29,6 @@ namespace ApplitoolsTutorial
         public void AfterEach()
         {
             eyes.AbortIfNotClosed();
-        }
-
-        static Bitmap GetImage(Uri uri)
-        {
-            var request = WebRequest.Create(uri);
-            using (var response = request.GetResponse())
-            using (var responseStream = response.GetResponseStream())
-            {
-                var imageStream = new MemoryStream();
-                responseStream.CopyTo(imageStream);
-                imageStream.Position = 0;
-
-                return new Bitmap(imageStream);
-            }
         }
     }
 }
